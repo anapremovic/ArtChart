4b2dab2892a9eab84ecd9fd6aa60045275e3d304
Nicholas Turenne <nbt1@sfu.ca>
2024-11-03 21:33:32 -0800

    Began working on the UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.lab.artchart"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.lab.artchart"
--- app/src/main/java/com/lab/artchart/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/lab/artchart/ui/search/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package com.lab.artchart.ui.gallery
+package com.lab.artchart.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is search Fragment"
     }
     val text: LiveData<String> = _text
 }
