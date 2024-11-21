17937786789d90e7705ffec1e58c00971c3f6fa3
apa109 <apa109@sfu.ca>
2024-11-21 12:50:11 -0800

    Add Firebase authentication dependency and activity dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.storage)
     implementation(libs.picasso)
+    implementation(libs.androidx.activity)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
