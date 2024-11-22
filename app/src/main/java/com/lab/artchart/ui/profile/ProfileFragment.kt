9493aaa80b8007bf1feaf7d0e65f5803b4f29d77
apa109 <apa109@sfu.ca>
2024-11-21 20:31:38 -0800

    Add sign up and sign in pages to ProfileFragment

--- app/src/main/java/com/lab/artchart/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/lab/artchart/ui/profile/ProfileFragment.kt
@@ -1,25 +1,32 @@
 package com.lab.artchart.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.lab.artchart.databinding.FragmentProfileBinding
+import com.lab.artchart.ui.userAuthentication.SignInActivity
+import com.lab.artchart.ui.userAuthentication.SignUpActivity
 
 class ProfileFragment : Fragment() {
-
     private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val homeViewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        // set up buttons that take you to sign up and sign in pages
+        binding.signUp.setOnClickListener {
+            startActivity(Intent(activity, SignUpActivity::class.java))
+        }
+        binding.signIn.setOnClickListener {
+            startActivity(Intent(activity, SignInActivity::class.java))
+        }
 
         return root
     }
