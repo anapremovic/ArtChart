1ac05124fd74658170e154e5d4b2aaa9bc4f75f4
Nicholas Turenne <nbt1@sfu.ca>
2024-11-22 19:24:25 -0800

    Began work on the artInfo activity

---
+++ app/src/main/java/com/lab/artchart/ui/search/ArtInfoActivity.kt
@@ -0,0 +1,14 @@
+package com.lab.artchart.ui.search
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lab.artchart.R
+
+class ArtInfoActivity: AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_art_info)
+    }
+
+}
