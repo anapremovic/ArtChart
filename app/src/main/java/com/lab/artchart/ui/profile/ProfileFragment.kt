823819d394d4de06991028c8a7354d8ce5fce467
apa109 <apa109@sfu.ca>
2024-11-23 23:48:34 -0800

    Add sign out confirmation dialog

--- app/src/main/java/com/lab/artchart/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/lab/artchart/ui/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.lab.artchart.ui.profile
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.google.firebase.auth.FirebaseUser
 import com.lab.artchart.R
 import com.lab.artchart.database.UserAuthenticationViewModel
 import com.lab.artchart.databinding.FragmentProfileBinding
@@ -25,25 +25,48 @@ class ProfileFragment : Fragment() {
         val root: View = binding.root
         userAuthenticationViewModel = (activity as MainActivity).userAuthenticationViewModel
 
-        userAuthenticationViewModel.currentUser.observe(viewLifecycleOwner) { user ->
-            if (user != null) {
-                setUserProfileInformation(user)
-            } else {
-                Log.w("PROFILE_FRAG", "No currently authenticated user")
-            }
-        }
+        // update UI
+        setUserProfileInformation()
+
+        // button listeners
         binding.signOutButton.setOnClickListener {
-            userAuthenticationViewModel.signOut()
-            Toast.makeText(requireContext(), "Signed out", Toast.LENGTH_LONG).show()
-            findNavController().navigate(R.id.nav_signIn)
+            openSignOutConfirmationDialog()
         }
 
         return root
     }
 
-    private fun setUserProfileInformation(user: FirebaseUser) {
-        // TODO: set username text
-        binding.emailText.text = user.email.toString()
+    // update UI with currently signed in user
+    private fun setUserProfileInformation() {
+        userAuthenticationViewModel.currentUser.observe(viewLifecycleOwner) { user ->
+            if (user != null) {
+                // TODO: set username text
+                binding.emailText.text = user.email.toString()
+            } else {
+                // unexpected behaviour - fragment should only be accessible when there is a user signed in
+                Log.w("PROFILE_FRAG", "No currently authenticated user")
+            }
+        }
+    }
+
+    // confirmation dialog
+    private fun openSignOutConfirmationDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Confirm Sign Out")
+        builder.setMessage("Are you sure you want to sign out?")
+
+        // sign user out and navigate back to sign in page
+        builder.setPositiveButton("Yes") { _, _ ->
+            userAuthenticationViewModel.signOut()
+            Toast.makeText(requireContext(), "Signed out", Toast.LENGTH_LONG).show()
+            findNavController().navigate(R.id.nav_signIn)
+        }
+
+        builder.setNegativeButton("No") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
     }
 
     override fun onDestroyView() {
