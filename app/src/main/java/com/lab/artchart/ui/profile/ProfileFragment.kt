c8edd876326c60dbeb8911e7817058496f9e8535
apa109 <apa109@sfu.ca>
2024-11-23 20:05:57 -0800

    Update ProfileFragment buttons to reflect functionality

--- app/src/main/java/com/lab/artchart/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/lab/artchart/ui/profile/ProfileFragment.kt
@@ -1,14 +1,11 @@
 package com.lab.artchart.ui.profile
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.lab.artchart.databinding.FragmentProfileBinding
-import com.lab.artchart.ui.userAuthentication.SignInActivity
-import com.lab.artchart.ui.userAuthentication.SignUpActivity
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
@@ -19,14 +16,6 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        // set up buttons that take you to sign up and sign in pages
-        binding.signUp.setOnClickListener {
-            startActivity(Intent(activity, SignUpActivity::class.java))
-        }
-        binding.signIn.setOnClickListener {
-            startActivity(Intent(activity, SignInActivity::class.java))
-        }
 
         return root
     }
