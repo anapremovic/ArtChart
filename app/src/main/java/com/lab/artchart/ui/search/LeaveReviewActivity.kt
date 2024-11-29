899fb07572758cd11e577d611050eebfe2f08dbf
apa109 <apa109@sfu.ca>
2024-11-29 15:13:55 -0800

    Use binding pattern in LeaveReviewActivity for usability

--- app/src/main/java/com/lab/artchart/ui/search/LeaveReviewActivity.kt
+++ app/src/main/java/com/lab/artchart/ui/search/LeaveReviewActivity.kt
@@ -1,36 +1,32 @@
 package com.lab.artchart.ui.search
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.lab.artchart.R
+import com.lab.artchart.databinding.ActivityLeaveReviewBinding
 import com.squareup.picasso.Picasso
 
 class LeaveReviewActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityLeaveReviewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_leave_review)
+        binding = ActivityLeaveReviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val imageUrl = intent.getStringExtra("imageUrl")
 
-        val artImage = findViewById<ImageView>(R.id.artwork_review_image)
+        val artImage = binding.artworkReviewImage
         Picasso.get().load(imageUrl).into(artImage)
 
-        val backButton = findViewById<LinearLayout>(R.id.back_button_review)
-        backButton.setOnClickListener {
+        binding.backButtonReview.setOnClickListener {
             finish()
         }
 
-        val cancelReviewButton = findViewById<Button>(R.id.cancel_review_button)
-        cancelReviewButton.setOnClickListener {
+        binding.cancelReviewButton.setOnClickListener {
             finish()
         }
 
-        val submitReviewButton = findViewById<Button>(R.id.submit_review_button)
-        submitReviewButton.setOnClickListener {
+        binding.submitReviewButton.setOnClickListener {
             //TODO: Send review to database
         }
     }
