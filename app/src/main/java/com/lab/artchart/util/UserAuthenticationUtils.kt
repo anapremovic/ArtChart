8cde907c06fd5bec0ad74c72d681a54df7ce46de
apa109 <apa109@sfu.ca>
2024-11-29 15:48:42 -0800

    Create helper function to navigate to sign in page as it will be used in LeaveReviewActivity

--- app/src/main/java/com/lab/artchart/util/UserAuthenticationUtils.kt
+++ app/src/main/java/com/lab/artchart/util/UserAuthenticationUtils.kt
@@ -1,12 +1,24 @@
 package com.lab.artchart.util
 
+import android.content.Context
+import android.content.Intent
 import android.text.InputType
 import android.util.Patterns
 import android.widget.CheckBox
 import android.widget.EditText
+import com.lab.artchart.ui.MainActivity
 
 // helper functions for user authentication
 object UserAuthenticationUtils {
+    // go back to sign in screen when user authentication is necessary for current action
+    fun navigateToSignInScreenFromActivity(context: Context) {
+        val intent = Intent(context, MainActivity::class.java)
+        intent.putExtra("NAVIGATE_TO", "SignInFragment")
+        // ensure we don't create new instances of MainActivity and fragments
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        context.startActivity(intent)
+    }
+
     // show and hide text depending on check box status
     fun handleShowPasswordCheckBox(passwordFields: List<EditText>, showPasswordCheckBox: CheckBox) {
         showPasswordCheckBox.setOnCheckedChangeListener { _, isChecked ->
@@ -19,6 +31,8 @@ object UserAuthenticationUtils {
             }
         }
     }
+
+    // verify minimum requirements for various fields and show user feedback if requirements not met
 
     fun verifyUsernameRequirements(username: String, usernameInput: EditText): Boolean {
         if (username.isBlank()) {
