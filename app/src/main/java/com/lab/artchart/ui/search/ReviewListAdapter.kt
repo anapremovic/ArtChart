b0196075171385e5e138da9f4ee13bfc871c3e27
apa109 <apa109@sfu.ca>
2024-12-01 11:49:10 -0800

    Implement fetching reviews from database

--- app/src/main/java/com/lab/artchart/ui/search/ReviewListAdapter.kt
+++ app/src/main/java/com/lab/artchart/ui/search/ReviewListAdapter.kt
@@ -1,12 +1,15 @@
 package com.lab.artchart.ui.search
 
 import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.lab.artchart.R
 import com.lab.artchart.database.Review
 
@@ -16,7 +19,7 @@ class ReviewListAdapter(private val context: Context, private var reviewList: Li
     }
 
     override fun getItem(position: Int): Any {
-        return reviewList.get(position)
+        return reviewList[position]
     }
 
     override fun getItemId(position: Int): Long {
@@ -24,18 +27,28 @@ class ReviewListAdapter(private val context: Context, private var reviewList: Li
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View = View.inflate(context, R.layout.layout_adapter_review, null)
-
-        val userImage = view.findViewById<ImageView>(R.id.user_profile_image)
-        val username = view.findViewById<TextView>(R.id.username)
-
-        // TODO: Set the userImage, usernamear with user info
+        val view: View = convertView ?: LayoutInflater.from(context).inflate(R.layout.layout_adapter_review, parent, false)
+        val review = reviewList[position]
 
         val ratingBar = view.findViewById<RatingBar>(R.id.review_rating_bar)
-        ratingBar.rating = reviewList[position].rating!!
-        val review = view.findViewById<TextView>(R.id.review_text)
-        review.text = reviewList[position].comment
+        ratingBar.rating = review.rating!!
+        val comment = view.findViewById<TextView>(R.id.review_text)
+        comment.text = review.comment
+        if (!review.profilePictureUrl.isNullOrEmpty()) {
+            val userImage = view.findViewById<ImageView>(R.id.user_profile_image)
+            Glide.with(context)
+                .load(review.profilePictureUrl)
+                .circleCrop()
+                .into(userImage)
+        }
+        val username = view.findViewById<TextView>(R.id.username_text)
+        username.text = review.username
 
         return view
+    }
+
+    fun replace(newReviewList: List<Review>) {
+        Log.d("REVIEW_LIST_ADAPTER", "Replacing with: $newReviewList")
+        reviewList = newReviewList
     }
 }
--- app/src/main/java/com/lab/artchart/ui/search/ViewReviewsActivity.kt
+++ app/src/main/java/com/lab/artchart/ui/search/ViewReviewsActivity.kt
@@ -1,39 +1,64 @@
 package com.lab.artchart.ui.search
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.lab.artchart.database.Review
+import androidx.lifecycle.ViewModelProvider
+import com.lab.artchart.database.ReviewViewModel
 import com.lab.artchart.databinding.ActivityViewReviewBinding
 
 class ViewReviewsActivity: AppCompatActivity() {
-
     private lateinit var binding: ActivityViewReviewBinding
+    private lateinit var reviewViewModel: ReviewViewModel
 
     // List views
     private lateinit var artListView: ListView
-    private lateinit var arrayList: ArrayList<Review>
     private lateinit var arrayAdapter: ReviewListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityViewReviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        reviewViewModel = ViewModelProvider(this)[ReviewViewModel::class.java]
 
-        println("ArtChart1: opened")
+        // set up list view
+        artListView = binding.reviewListView
+        arrayAdapter = ReviewListAdapter(this, listOf())
+        artListView.adapter = arrayAdapter
 
+        // extract extras
+        val artId = intent.getStringExtra("artId")
+        val uid = intent.getStringExtra("uid")
+
+        // update list view depending on if entered from ArtInfo page or ProfileFragment
+        if (artId != null) {
+            Log.d("VIEW_REVIEWS_ACT", "Displaying reviews for art with ID $artId")
+            reviewViewModel.loadAllReviewsForArtwork(artId)
+            getReviewListForArt()
+        } else if (uid != null) {
+            Log.d("VIEW_REVIEWS_ACT", "Displaying reviews for user with ID $uid")
+            reviewViewModel.loadAllReviewsForUser(uid)
+            getReviewListForUser()
+        }
+
+        // button listeners
         binding.backButtonViewReview.setOnClickListener {
             finish()
         }
+    }
 
-        val artId = intent.getStringExtra("artId")
+    private fun getReviewListForArt() {
+        reviewViewModel.allReviewsForArtwork.observe(this) { reviews ->
+            arrayAdapter.replace(reviews)
+            arrayAdapter.notifyDataSetChanged()
+        }
+    }
 
-        artListView = binding.reviewListView
-
-        arrayList = ArrayList()
-        arrayAdapter = ReviewListAdapter(this, arrayList)
-        artListView.adapter = arrayAdapter
-
-        // TODO: Set up the reviews using the database call and put them in the adapter using arrayAdapter.replace(the list)
+    private fun getReviewListForUser() {
+        reviewViewModel.allReviewsForUser.observe(this) { reviews ->
+            arrayAdapter.replace(reviews)
+            arrayAdapter.notifyDataSetChanged()
+        }
     }
 }
