01639f68cc1c20feae79098108406616663193a6
apa109 <apa109@sfu.ca>
2024-12-01 11:48:23 -0800

    Add username and profile picture to Review

--- app/src/main/java/com/lab/artchart/database/Review.kt
+++ app/src/main/java/com/lab/artchart/database/Review.kt
@@ -4,5 +4,7 @@ data class Review (
     val comment: String? = null,
     val rating: Float? = null,
     val artId: String? = null,
-    val uid: String? = null
+    val uid: String? = null,
+    val username: String? = null,
+    var profilePictureUrl: String? = null
 )
--- app/src/main/java/com/lab/artchart/ui/search/LeaveReviewActivity.kt
+++ app/src/main/java/com/lab/artchart/ui/search/LeaveReviewActivity.kt
@@ -6,28 +6,35 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.lab.artchart.database.Review
 import com.lab.artchart.database.ReviewViewModel
+import com.lab.artchart.database.UserViewModel
 import com.lab.artchart.databinding.ActivityLeaveReviewBinding
 import com.squareup.picasso.Picasso
 
 class LeaveReviewActivity: AppCompatActivity() {
     private lateinit var binding: ActivityLeaveReviewBinding
     private lateinit var reviewViewModel: ReviewViewModel
+    private lateinit var userViewModel: UserViewModel
 
     // art and review info
     private var imageUrl: String? = null
     private var artId: String? = null
     private var uid: String? = null
+    private var username: String? = null
+    private var profilePictureUrl: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLeaveReviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
         reviewViewModel = ViewModelProvider(this)[ReviewViewModel::class.java]
+        userViewModel = ViewModelProvider(this)[UserViewModel::class.java]
 
         // Extract info from intent
         imageUrl = intent.getStringExtra("imageUrl")
         artId = intent.getStringExtra("artId")
         uid = intent.getStringExtra("uid")
+        uid?.let { userViewModel.fetchUserByUid(it) }
+        getUsernameAndProfilePicture()
 
         // load image
         val artImage = binding.artworkReviewImage
@@ -49,12 +56,21 @@ class LeaveReviewActivity: AppCompatActivity() {
         }
     }
 
+    private fun getUsernameAndProfilePicture() {
+        userViewModel.user.observe(this) {
+            username = it?.username
+            profilePictureUrl = it?.profilePictureUrl
+        }
+    }
+
     private fun saveReview() {
         val review = Review (
             binding.longEditText.text.toString(),
             binding.reviewRatingBar.rating,
             artId,
-            uid
+            uid,
+            username,
+            profilePictureUrl
         )
 
         reviewViewModel.saveReview(review)
